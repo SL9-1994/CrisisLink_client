79c5fa48e2bf2afda14266710c3b2a04b5c07e56
SL9-1994 <oktm1220@gmail.com>
2024-01-22 01:05:29 +0900

    add:✨ top-pageの比率を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import NavMenu from "./components/NavMenu";
 
 export default function Home() {
   return (
     <>
-      <main>Init</main>
+      <main className="flex h-screen w-screen">
+        {/* leftConponent */}
+        <div className="bg-gray-100 h-full" style={{ flex: "2.5" }}>
+          <NavMenu></NavMenu>
+        </div>
+        {/* centerComponent */}
+        <div className="bg-gray-300 h-full" style={{ flex: "4.5" }}></div>
+        {/* rightComponent */}
+        <div className="bg-gray-200 h-full" style={{ flex: "3" }}></div>
+      </main>
     </>
   );
 }
