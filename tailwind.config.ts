40ee96b32c3ffbbea32737071d5176ff7e578d87
SL9-1994 <oktm1220@gmail.com>
2024-01-22 14:17:17 +0900

    add:✨ カスタムCSSを追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      flex: {
+        "3": "3",
+        "4": "4",
+      },
     },
   },
   plugins: [],
